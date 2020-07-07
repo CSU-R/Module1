6693f2a2c82f9d4d32ab9aad3a595ba3987ef3ed
Alex Fout <fout.alex@gmail.com>
2020-07-07 12:06:42 -0600

    created script to make feedback blocks with prefilled links, and put a lot of feedback blocks where they need to go

---
+++ rscripts/create_feedback_blocks.R
@@ -0,0 +1,39 @@
+library(stringr)
+
+section_headings <- c(
+  "Welcome",
+  "Course Preliminaries",
+  "Course Topics & Syllabus", 
+  "Running your first R Code",
+  "What do you hope to get out of this course",
+  "What is R",
+  "The R Community",
+  "Computer Basics",
+  "Install R & RStudio",
+  "Successfull Installation",
+  "Running Code in RStudio Console",
+  "Workspace Setup",
+  "Programming Preliminaries",
+  "Data Types",
+  "Data Structures",
+  "R Objects"
+)
+link_prefix <- "https://docs.google.com/forms/d/e/1FAIpQLSePQZ3lIaCIPo9J2owXImHZ_9wBEgTo21A0s-A1ty28u4yfvw/viewform?entry.1684471501="
+
+block1 <- "```{block, type='feedback'}
+Any feedback for this section? Click [here]("
+
+block2 <- 
+")
+```"
+
+
+
+blocks <- list()
+for(i in 1:length(section_headings)){
+  sec <- section_headings[[i]]
+  blocks[[i]] <- paste0(block1, link_prefix, URLencode(sec), block2)
+  print(sec)
+  cat(blocks[[i]], "\n\n")
+}
+
