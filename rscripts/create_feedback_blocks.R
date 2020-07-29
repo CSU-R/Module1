8b7c24d2a59e9b7e4073235a52b2d3452070d865
Alex Fout <fout.alex@gmail.com>
2020-07-29 00:49:25 -0600

    added new blocks.

--- rscripts/create_feedback_blocks.R
+++ rscripts/create_feedback_blocks.R
@@ -27,6 +27,12 @@ section_headings <- c(
   "Basic Plotting",
   "Vignettes"
 )
+section_headings <- c(
+  "Basic Control Flow",
+  "Advanced Control Flow",
+  "Writing Functions",
+  "R Packages"
+)
 link_prefix <- "https://docs.google.com/forms/d/e/1FAIpQLSePQZ3lIaCIPo9J2owXImHZ_9wBEgTo21A0s-A1ty28u4yfvw/viewform?entry.1684471501="
 
 block1 <- "```{block, type='feedback'}
